7ac22592df3b5825480a6f4be4cf9e1cb8ed3ae3
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-06 11:02:04 +0200

    close #9 , estructura de la clase creada

---
+++ src/videojuego.js
@@ -0,0 +1,21 @@
+class Videojuego{
+	
+
+	constructor (nomVideojuego, desc, fecha){
+		this.nombreVideojuego = nomVideojuego;
+		this.descuento = desc;
+		this.fechaLanzamiento = fecha;
+		var reservado = false;
+	}
+
+
+	consultarFechaLanzamiento(){
+	}
+
+	consultarDescuento(){
+	}
+
+	reservarVideojuego(){
+	}
+
+}
