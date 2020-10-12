89a012494a5d3ca09c600699172ffb0b71571406
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-12 17:54:43 +0200

    close #18, añadido videojuego.rb

--- src/videojuego.rb
+++ src/videojuego.rb
@@ -4,25 +4,27 @@ class Videojuego
     attr_accessor:fechaLanzamiento
     attr_accessor:descuento
     attr_accessor:reservado
+    attr_reader:precioInicial
         
         
-        def initialize (nombre, fecha, desc)
+        def initialize (nombre, fecha, desc, precio)
             @nombreVideojuego = nombre
             @fechaLanzamiento = fecha
             @descuento = desc
             @reservado = false
+	    @precioInicial = precio
             
         end
         
         
         
-        def consultarFechaLanzamiento()                                           # Corresponde a la HU01
+        def consultarDiasRestantes()                                              # Corresponde a la HU01
             
         end
         
         
         
-        def consultarDescuento()                                                  # Corresponde a la HU02
+        def consultarPrecioFinal()                                                # Corresponde a la HU02
             
         end
         
