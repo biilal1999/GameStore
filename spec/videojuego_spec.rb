7ec1415bd04e0c03857381ee16fcee4714e5ae56
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-13 20:09:09 +0200

    Avanzo #22 , implemento test básico para HU03

--- spec/videojuego_spec.rb
+++ spec/videojuego_spec.rb
@@ -14,6 +14,15 @@ describe Videojuego do
 			expect(vi.consultarPrecioFinal()).to eq(37.5)
 		end
 	end
-	
+
+	describe '#consultarMediaEdad' do
+		vi.edadesVentas.push(18)
+		vi.edadesVentas.push(19)
+		vi.edadesVentas.push(23)
+		
+		it 'media de edad' do
+			expect(vi.consultarMediaEdad()).to eq(20)
+		end	
+	end	
 end
 
