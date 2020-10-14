ae7d442f48d0805b853475e4da8017a85632d4bf
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-14 16:10:47 +0200

    Avanzo #21 , añado comentarios al fichero

--- spec/videojuego_spec.rb
+++ spec/videojuego_spec.rb
@@ -1,44 +1,39 @@
 require '../src/Videojuego.rb'
 
-describe Videojuego do
-	vi = Videojuego.new("No se", Date.today, 0.25, 50)
+describe Videojuego do													# Testear clase Videojuego
+	vi = Videojuego.new("No se", Date.today, 0.25, 50)						
 	vidjuego = Videojuego.new("No se", Date.new(2020, 10, 05), 0.25, 50)
 
-	describe '#consultarDiasRestantes' do
+	describe '#consultarDiasRestantes' do										# Testear método consultarDiasRestantes()  HU01
 		it 'excepción de dias restantes' do
-			expect(vidjuego.consultarDiasRestantes()).not_to be_a_kind_of(Numeric)
+			expect(vidjuego.consultarDiasRestantes()).not_to be_a_kind_of(Numeric)					# Testear excepción
 		end
-	end
-
-	describe '#consultarDiasRestantes' do
 		
 		it 'dias restantes' do
-			expect(vi.consultarDiasRestantes()).to eq(0)
+			expect(vi.consultarDiasRestantes()).to eq(0)								# Testear caso correcto
 		end
 	end
 
-	describe '#consultarPrecioFinal' do
+	describe '#consultarPrecioFinal' do										# Testear método consultarPrecioFinal()  HU02
 		it 'precio final' do
-			expect(vi.consultarPrecioFinal()).to eq(37.5)
+			expect(vi.consultarPrecioFinal()).to eq(37.5)								
 		end
 	end
 
 
-	describe '#consultarMediaEdad' do
+	describe '#consultarMediaEdad' do										# Tester método consultarMediaEdad() HU03
 		vi.addVenta(18)
 		vi.addVenta(19)
 		vi.addVenta(23)
 		
 		it 'media de edad' do
-			expect(vi.consultarMediaEdad()).to eq(20)
+			expect(vi.consultarMediaEdad()).to eq(20)								# Testear caso correcto
 		end	
-	end
 
-	describe '#consultarMediaEdad' do
 		it 'excepción de media de edad' do
-			expect(vidjuego.consultarMediaEdad()).not_to be_a_kind_of(Float)
+			expect(vidjuego.consultarMediaEdad()).not_to be_a_kind_of(Float)					# Testear excepción
 		end	
-	end
 	
+	end
 end
 
