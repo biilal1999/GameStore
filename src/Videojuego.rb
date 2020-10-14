1dc09e024f54d1f142fcbd706206916739b28e54
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-14 12:54:05 +0200

    Avanzo #14 , mejoro implemenación de media de edades

---
+++ src/Videojuego.rb
@@ -0,0 +1,71 @@
+
+require 'date'
+
+class Videojuego
+	    
+	attr_reader:nombreVideojuego
+	    attr_reader:fechaLanzamiento
+	    attr_reader:descuento
+	    attr_reader:edadesVentas
+	    attr_reader:precioInicial
+		
+		
+		def initialize (nombre, fecha, desc, precio)
+		    @nombreVideojuego = nombre
+		    @fechaLanzamiento = fecha
+		    @descuento = desc
+		    @edadesVentas = []
+		    @precioInicial = precio
+		end
+		
+		
+		
+		def consultarDiasRestantes()                                              # Corresponde a la HU01
+		    now = Date.today
+		    dias_restantes = @fechaLanzamiento - now
+
+		    begin
+		       dias = dias_restantes.to_i
+		       raise 'Ya salió el juego' if dias < 0
+
+		    rescue => dias
+		    end
+
+		    return dias
+		end
+		
+		
+		
+		def consultarPrecioFinal()                                                # Corresponde a la HU02
+		    precioFinal = @precioInicial - (@precioInicial * @descuento)
+
+		    return precioFinal
+		end
+		
+		
+		
+		def consultarMediaEdad() 	                                          # Corresponde a la HU03
+		    suma = 0
+		     
+		    begin
+		       ventas = @edadesVentas.length
+		       raise 'No hay ventas de momento' if ventas == 0
+
+		       for edad in @edadesVentas
+			   suma = suma + edad
+		       end
+			    
+		       media = ((suma * 1.0) / ventas)
+
+		    rescue => media
+	 	    end
+		    
+		    return media
+		end
+	    
+		
+		def addVenta (edad)                                                       # Añade edades al array cuando se realiza una venta
+		    @edadesVentas.push(edad)
+		end
+
+end
