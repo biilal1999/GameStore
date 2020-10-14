f47b5e23bc1cf6fd20294dd162b642625600c277
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-14 18:55:32 +0200

    close #21 , termino tests significativos para HU02

--- spec/videojuego_spec_significativos.rb
+++ spec/videojuego_spec_significativos.rb
@@ -21,6 +21,19 @@ describe Videojuego do													# Testear clase Videojuego
 	end
 
 
-	
+	describe '#consultarMediaEdad' do										# Tester método consultarMediaEdad() HU03
+		vi.addVenta(18)
+		vi.addVenta(19)
+		vi.addVenta(23)
+		
+		it 'media de edad' do
+			expect(vi.consultarMediaEdad()).to eq(20)								# Testear caso correcto
+		end	
+
+		it 'excepción de media de edad' do
+			expect(vidjuego.consultarMediaEdad()).not_to be_a_kind_of(Float)					# Testear excepción
+		end	
+
+	end
 end
 
