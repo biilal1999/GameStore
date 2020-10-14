ee6c4f159c803289be517fc7e0252626851a8519
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-10-14 17:00:14 +0200

    Avanzo #33 , añado test básicos/unitarios para Videojuego.rb

--- spec/videojuego_spec_basicos.rb
+++ spec/videojuego_spec_basicos.rb
@@ -2,23 +2,40 @@ require '../src/Videojuego.rb'
 require 'date'
 
 
-describe Videojuego do
+describe Videojuego do										# Testeamos métodos básicos de GET
 	vi = Videojuego.new("COD", Date.new(2020, 10, 18), 0.15, 70)
 
-	describe '#nombreVideojuego' do
+	describe '#nombreVideojuego' do										# Nombre del videojuego
 		it 'nombre del videojuego' do
-			expect(vi.nombreVideojuego).to eq('COD')
+			expect(vi.nombreVideojuego()).to eq('COD')
 		end
 	end
 
 
-	describe '#fechaLanzamiento' do
+	describe '#fechaLanzamiento' do										# Fecha de lanzamiento
 		it 'fecha de lanzamiento' do
-			expect((vi.fechaLanzamiento).to_s).to eq('2020-10-18')
+			expect((vi.fechaLanzamiento()).to_s).to eq('2020-10-18')
 		end
 	end
 
 	
-	describe '#descuentos' do
-		it '
+	describe '#descuento' do										# Descuento
+		it 'descuento sobre 1' do
+			expect(vi.descuento()).to eq(0.15)
+		end
+	end
+
+	
+	describe '#edadesVentas' do										# Array de edades de compradores
+		it 'número de ventas' do
+			expect(vi.edadesVentas.length).to eq(0)
+		end
+	end
+
+
+	describe '#precioInicial' do										# Precio inicial del juego
+		it 'precio inicial del videojuego' do
+			expect(vi.precioInicial()).to eq(70)
+		end
+	end
 end
