8ce9b0862c08c340708b04f86c7f8ddd86d1e01d
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-21 19:27:55 +0100

    Avanzo #13 y #77 , continúo implementando la función de precio.rb [skip ci]

--- api/precio.rb
+++ api/precio.rb
@@ -6,6 +6,21 @@ Handler = Proc.new do |req, res|
 	cadena = Array.new
 	juego = req.query['v']
 	
+	if juego == nil
+		cadena = {error: "Tiene que enviar por parámetro el nombre del videojuego"}
+
+	else
+		info = JSON.parse(File.read(File.join(File.dirname(__FILE__), 'info.json')))
+
+		encontrado = false
+		vid = nil
 	
+		for vj in info["videojuegos"]
+
+			if juego.casecmp(vj["nombreVideojuego"])
+				vid = vj
+			end
+		end
+
 
 
