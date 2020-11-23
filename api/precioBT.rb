8a5087a72b3f7b9c69bbf5af37902611796a3976
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-23 17:48:47 +0100

    con json a ver hostias [skip ci]

--- api/precioBT.rb
+++ api/precioBT.rb
@@ -9,6 +9,6 @@ Handler = Proc.new do |req, res|
 	mensaje = "holaaa"
 
 	res.status = 200
-	res['Content-Type'] = 'text/text; charset=utf-8'
+	res['Content-Type'] = 'application/json; charset=utf-8'
 	res.body = mensaje.to_json
 end
