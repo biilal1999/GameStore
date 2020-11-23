fe0e02a262ee5ee2ee610e42a648627ac0fcb121
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-23 17:02:20 +0100

    a ver ahora [skip ci]

--- api/precioBT.rb
+++ api/precioBT.rb
@@ -11,11 +11,9 @@ Handler = Proc.new do |req, res|
 	id = recibo["message"]["chat"]["id"]
 
 	vuelta = {msg: mensaje, chat: id}
-	url = "https://api.telegram.org"
-	uri = URI(url)
 
-	res = Net::HTTP::Get.new("https://api.telegram.org/bot#{TOKEN}/sendMessage", {'Content-Type' => 'application/json; charset=utf-8'})
 	res.status = 200
+	res['Content-Type'] = 'text/text; charset=utf-8'
 	res.body = vuelta.to_json
-	
+	res = Net::HTTP::Get.new("https://api.telegram.org/bot#{TOKEN}/sendMessage")
 end
