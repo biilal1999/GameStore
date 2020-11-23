487c8d81827902829935e4100063d5c3d3f1d98a
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-23 19:13:15 +0100

    ccc [skip ci]

--- functions/puntos.js
+++ functions/puntos.js
@@ -2,24 +2,24 @@ const request = require('request-promise');
 const info = require('./info.json');
 
 
-exports.handler = async function (event, context){
-	try{
-		let body = JSON.parse(event.body);
-		let {chat, text} = body.message;
-		console.log(body.message);
+	exports.handler = async function (event, context){
+		try{
+			let body = JSON.parse(event.body);
+			let {chat, text} = body.message;
+			console.log(body.message);
 
-		let cadena = "eeey";
+			let cadena = "eeey";
 
-		return {
-			statusCode: 200,
-			body: JSON.stringify({text: cadena, method: 'sendMessage', chat_id:chat.id})
-			headers:{
-				'Content-Type': 'application/json'
-			}
-		};
+			return {
+				statusCode: 200,
+				body: JSON.stringify({text: cadena, method: 'sendMessage', chat_id:chat.id})
+				headers:{
+					'Content-Type': 'application/json'
+				}
+			};
+		}
+
+		catch(erroe){
+		}
 	}
-
-	catch(erroe){
-	}
-}
 
