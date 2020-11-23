a9db33e9da110a7bf9c4466c5e177fbd1df3bff1
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-23 17:59:58 +0100

    con json a ver hostias ultimo intento [skip ci]

--- api/precioBT.rb
+++ api/precioBT.rb
@@ -7,8 +7,9 @@ TOKEN = ENV["TELEGRAM_BOT"]
 Handler = Proc.new do |req, res|
 
 	mensaje = "holaaa"
-	
-	vuelta = {msg: mensaje}
+	recibo = JSON.parse(req.body)
+	id = recibo["message"]["chat"]["id"]
+	vuelta = {text: mensaje, chat_id: id}
 	res.status = 200
 	res['Content-Type'] = 'application/json; charset=utf-8'
 	res.body = vuelta.to_json
