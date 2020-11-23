ea8f6e2b0da4d4c05b981d5a8fd9ae98823ec1c9
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-11-23 19:59:57 +0100

    Avanzo #82 , implemento función para el bot [skip ci]

--- functions/puntos.js
+++ functions/puntos.js
@@ -2,24 +2,31 @@ const request = require('request-promise');
 const info = require('./info.json');
 
 
-	exports.handler = async function (event, context){
-		try{
-			let body = JSON.parse(event.body);
-			let {chat, text} = body.message;
-			console.log(body.message);
+Function calculaPuntos(cliente){
 
-			let cadena = "eeey";
+}
 
-			return {
-				statusCode: 200,
-				body: JSON.stringify({text: cadena, method: 'sendMessage', chat_id:chat.id}),
-				headers:{
-					'Content-Type': 'application/json'
-				}
-			};
-		}
+exports.handler = async function (event, context){
+	let body = JSON.parse(event.body);
+	let {chat, msg} = body.message;
 
-		catch(error){
-		}
+	if (msg == "/puntos"){
+		cadena = "¿Bilal? ¿JJ? ¿Mario? ¿Raúl? ¿Qué clientes quieres consultar?";
+		cadnena += "\n";
+		cadena = "Por ejemplo, /puntosBilal";
 	}
+
+	else{
+		cadena = "eeey";
+	}
+	
+
+	return {
+		statusCode: 200,
+		body: JSON.stringify({text: cadena, method: 'sendMessage', chat_id:chat.id}),
+		headers:{
+			'Content-Type': 'application/json'
+		}
+	};
+}
 
