a0e5be709b2098fa11d35c0c660ca5764459a50f
Mohamed Bilal Chekfeh Al Sabbagh <bilal99@correo.ugr.es>
2020-12-09 19:14:08 +0100

    Avanzo #87 , sigo con la API ... [skip ci]

--- src/Admin.rb
+++ src/Admin.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'date'
 require_relative 'Videojuego.rb'
 require_relative 'Ciudades.rb'
 require_relative 'Tienda.rb'
@@ -10,18 +11,56 @@ class Admin
     attr_reader :tiendas
     attr_reader :clientes
     attr_reader :videojuegos
+    attr_reader :datos
 
     def initialize
+        @datos = JSON.parse(File.read(File.join(File.dirname(__FILE__), '../datos.json')))
         @tiendas = Array.new
 
-        @ciudades = Array.new
-        @ciudades.push("Granada")
-        @ciudades.push("Madrid")
-        @ciudades.push("Barcelona")
-        @ciudades.push("Sevilla")
-        @ciudades.push("Valencia")
-        @ciudades.push("Zaragoza")
-        @ciudades.push("Vigo")
+        for i in @datos["tiendas"]
+            fila = @tiendas.size
+            @tiendas[fila] = Array.new
+            ciu = i["ciudad"]
+            t = Tienda.new(ciu)
+            @tiendas[fila].push(t)
+            @tiendas[fila][1] = Array.new
+            @tiendas[fila][2] = Array.new
+
+            for j in i["videojuegos"]
+                nombre = j["nombreVideojuego"]
+                descuento = j["descuento"]
+                fecha = j["fechaLanzamiento"]
+                fecha = fecha.to_date
+                precio = j["precioInicial"]
+                puntos = j["puntos"]
+
+                v = Videojuego.new(nombre, fecha, descuento, precio, puntos)
+                @tiendas[fila][1].push(v)
+            end
+
+            for k in i["clientes"]
+                nom = k["nombre"]
+                codigo = k["codigo"]
+                vec = Array.new
+
+                for l in k["videojuegosComprados"]
+                    nombre = l["nombreVideojuego"]
+                    descuento = l["descuento"]
+                    fecha = l["fechaLanzamiento"]
+                    fecha = fecha.to_date
+                    precio = l["precioInicial"]
+                    puntos = l["puntos"]
+
+                    v = Videojuego.new(nombre, fecha, descuento, precio, puntos)
+                    vec.push(v)
+                end
+
+                cli = Cliente.new(nom, Date.today, t)
+                cli.videojuegosComprados = vec
+
+                @tiendas[fila][2].push(cli)
+            end
+        end
     end
 
     def existeTienda
